719869220879a91e65db511175a3ee02470ce2a1
jang-sch <60149315+jang-sch@users.noreply.github.com>
2021-08-01 20:27:44 -0700

    skeleton for odin. might need dos2unix [filename]

---
+++ calculator.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# #############################################################################
+# course: cmps3500
+# CLASS Project
+# RUBY IMPLEMENTATION OF A CUSTOM SCIENTIFIC CALCULATION
+# date: 07/15/2021
+# Student 1: Janneth Guarcas Garcia
+# Student 2: none
+# description: Implementation of a scientific calculator in Ruby
+# #############################################################################
+
+
+# #############################################################################
+# function definitions
+
+
+
+# #############################################################################
+# variables (may move this section)
+
+
+
+# #############################################################################
+# main script
